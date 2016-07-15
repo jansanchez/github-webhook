784c0a96cfbb3c65015ee1afaf642da786ef1ae2
Jan Sanchez <joejansanchez@gmail.com>
2016-07-15 00:22:35 -0500

    refactor(webhook): refactorizo el wh

--- github-webhook.js
+++ github-webhook.js
@@ -1,42 +1,43 @@
 #!/usr/bin/env node
 'use strict';
 
-var async = require('async'),
-    request = require('request');
+var async = require('async');
+var request = require('request');
 
 return function (context, req, res) {
-    var body = '';
-    var webtask_code;
+	var body = '';
 
-    async.series([
-        function (callback) {
-            // Collect payload of GitHub web hook
-            req.on('data', function (chunk) { body += chunk; });
-            req.on('end', function () {
-                try {
-                    body = JSON.parse(body);
-                    if (!body || typeof body !== 'object')
-                        throw new Error("Unexpected web hook payload.");
-                    if (!body.repository || typeof body.repository.full_name !== 'string')
-                        throw new Error("Repository not identified in web hook payload.");
-                }
-                catch (e) {
-                    return callback(e);
-                }
-                return callback();
-            });
-            req.on('error', callback);
-        },
-      ], function (error) {
-        try {
-            if (error) {
-                console.log('ERROR', error);
-                res.writeHead(500);
-                res.end(error.toString());
-            }
-        }
-        catch (e) {
-            // ignore
-        }
-    });
+	async.series([
+		function (callback) {
+			// Collect payload of GitHub web hook
+			req.on('data', function (chunk) {
+				body += chunk;
+			});
+			req.on('end', function () {
+				try {
+					body = JSON.parse(body);
+					if (!body || typeof body !== 'object') {
+						throw new Error('Unexpected web hook payload.');
+					}
+					if (!body.repository || typeof body.repository.full_name !== 'string') {
+						throw new Error('Repository not identified in web hook payload.');
+					}
+				} catch (err) {
+					return callback(err);
+				}
+				return callback();
+			});
+			req.on('error', callback);
+		}
+	], function (error) {
+		try {
+			if (error) {
+				console.log('ERROR', error);
+				res.writeHead(500);
+				res.end(error.toString());
+			}
+		} catch (err) {
+			// ignore
+		}
+	});
 };
