db62786e1a34a26ac9f531abdd7cedebb482ea71
Jan Sanchez <joejansanchez@gmail.com>
2016-07-17 22:30:02 -0500

    refactor(gw): refactorizo algo de codigo

--- github-webhook.js
+++ github-webhook.js
@@ -3,21 +3,12 @@ var app = require('express')();
 var bodyParser = require('body-parser');
 var port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  var json = JSON.stringify({
-   "from": "webhook"
- });
- res.writeHead(200, {"Content-Type": "application/json"});
- res.end(json);
-});
 
 app.post('/', function(req, res) {
   var json = JSON.stringify(req.body);
   var url = 'https://frontendlabs.io/api/';
-  console.log(json);
+
   request.post({url: url, form: json}, function(err, httpResponse, body){
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify(body));
