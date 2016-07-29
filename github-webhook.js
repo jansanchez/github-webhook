633b8fce0ffc545ab742163ba5378c6a7ceab666
Jan Sanchez <joejansanchez@gmail.com>
2016-07-29 17:27:53 -0500

    feat(project): agrego logica de uso de payload

--- github-webhook.js
+++ github-webhook.js
@@ -1,12 +1,18 @@
 var request = require('request');
 var app = require('express')();
 var bodyParser = require('body-parser');
+var BitbucketPayload = require('bitbucket-payload');
 var port = 3000;
 
 app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
-  var json = JSON.stringify(req.body);
+  var payload = req.body;
+
+  var bitbucketPayload = new BitbucketPayload(payload, 'push');
+  var newPayload = bitbucketPayload.getPurged();
+
+  var json = JSON.stringify(newPayload);
   var url = 'https://frontendlabs.io/api/';
 
   request.post({url: url, form: json}, function(err, httpResponse, body){
@@ -16,5 +22,5 @@ app.post('/', function(req, res) {
 });
 
 app.listen(port, function(){
-  console.log('Server running in localhost: ' + port);
+  console.log('Server running in localhost:' + port);
 });
